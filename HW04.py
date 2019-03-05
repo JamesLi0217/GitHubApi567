c4dd6f6ae29fc20dcb73da7688dd32d47951fdcd
JamesLi0217 <46985021+JamesLi0217@users.noreply.github.com>
2019-03-05 18:08:22 -0500

    Add files via upload

--- HW04.py
+++ HW04.py
@@ -7,47 +7,39 @@ import urllib.request
 import json
 
 
-
 def search_repo(user_id):
     '''  search repository name and its commits'''
-    
+
     URL = f"https://api.github.com/users/{user_id}/repos"
-    
-    repo = urllib.request.Request(URL)
-    
-    fhand = urllib.request.urlopen(repo).read().decode()
-    
+
+    fhand = urllib.request.urlopen(URL)
+
     js_str = json.loads(fhand)
-    
+
     n = len(js_str)
 
-    commits_num={}
+    commits_num = {}
 
     for i in range(n):
 
         repo_name = js_str[i]["name"]
-        
-        URL = f"https://api.github.com/repos/{user_id}/{repo_name}/commits"
-        
-        commits = urllib.request.Request(URL)
-        
-        fhand_commit = urllib.request.urlopen(commits).read().decode()
-        
+
+        URL_commit = f"https://api.github.com/repos/{user_id}/{repo_name}/commits"
+
+        fhand_commit = urllib.request.urlopen(URL_commit)
+
         js_str_commit = json.loads(fhand_commit)
-        
-        commits_num[repo_name]=len(js_str_commit)
+
+        commits_num[repo_name] = len(js_str_commit)
 
     return commits_num
 
 
 def main():
-    commit=search_repo("JamesLi0217")
+    commit = search_repo("JamesLi0217")
     for repo in commit:
         print(f"Repo: {repo} Number of commits: {commit[repo]}")
 
 
 if __name__ == "__main__":
     main()
-    
-    
-    
--- HW04_test.py
+++ HW04_test.py
@@ -3,16 +3,30 @@
 @Github: https://github.com/JamesLi0217
 @Date: 2019-02-19 01:19:34
 '''
+
 import unittest
+from unittest import mock
 from HW04 import search_repo
 
+
 class TEST_search_repo(unittest.TestCase):
-        '''test simple function search_repo()'''
-        def test_search_repo(self):
-                self.assertEqual(search_repo("JamesLi0217"),{"GitHubApi567": 15,"SSW555_TeamPrj" : 1,"SSW810" : 1,"Stevens": 1,"Stevens-SSW567" : 2,"Triangle567" : 7})
+    '''test simple function search_repo()'''
+    @mock.patch("urllib.request.urlopen")
+    def test_search_repo(self, mock_search_repo):
+        mock_search_repo.side_effect = [
+            '[{"name":"GitHubApi567"}, {"name":"SSW810"},{"name":"Stevens"},{"name":"Triangle567"}]',
+            '[{"foo":1},{"foo1":1},{"foo2":1},{"foo3":1},{"foo4":1},{"foo5":1},{"foo6":1},{"foo7":1},{"foo8":1}]',
+            '[{"boo":1},{"boo1":1},{"boo2":1}]',
+            '[{"bin":1},{"bin1":1},{"bin2":1},{"foo4":1},{"foo5":1},{"foo6":1},{"foo7":1},{"foo8":1}]',
+            '[{"bin":1},{"bin1":1},{"bin2":1},{"foo4":1},{"foo5":1}]',
+        ]
+
+        expect = {"GitHubApi567": 9, "SSW810": 3,"Stevens": 8, "Triangle567": 5}
+        result = search_repo("JamesLi0217")
+        self.assertEqual(result, expect)
 
 
 if __name__ == "__main__":
-    
+
     print('Running unit tests')
-    unittest.main()
+    unittest.main(exit=False, verbosity=2)
